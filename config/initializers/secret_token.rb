d1c046ae324393c93800f8c7e1e4917cd58c4a0c
Matthew Todd <matthew@matthewtodd.org>
2011-12-15 16:48:44 -0800

    Generate rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VenturaGraphicDesign::Application.config.secret_token = '5940d885d945cbce3a9564dc1c7a7bab9c469ac95f0b27b447239f9af16d147f9f86d5602b3ba12934639565c3fcd323d1783555722780501ad7a011fc52615a'
